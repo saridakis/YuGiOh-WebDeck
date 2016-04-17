a2e2071ff818977a033e367f1f01a4fad16de08a
Saridakis Antonis <antonis_saridakis@hotmail.com>
2016-04-17 22:07:29 +0300

    Some Notes...

--- YoGiOh/Scripts/yugioh.js
+++ YoGiOh/Scripts/yugioh.js
@@ -1,4 +1,5 @@
-﻿(function (window, angular, $) {    
+﻿//Private Stack
+(function (window, angular, $) {
     "use strict";
     function cardController($scope, $http, $timeout) {               
         
@@ -25,61 +26,62 @@
             { name: 'Void Seer' },
         ];
         
+        //Get info about the selected card.
         $scope.selectCard = function ($index) {
             $scope.selectedCard = $scope.cards[$index];
             var encodedCardName = encodeURI($scope.selectedCard.name);
             
             $http.get('https://jsonp.afeld.me/?url=http://yugiohprices.com/api/get_card_prices/' + encodedCardName).then(function (response) {
-                $timeout(function () {                    
-                    //console.log(response);
-                    $scope.selectedCard.cardPrices = response.data.data;
-                    //$('#pricesTab a:first').tab('show');
-                    //$('#pricesTab a:last').tab('show');
+                $timeout(function () {
+                    $scope.selectedCard.cardPrices = response.data.data;                
                 }, 100);
             }, function (response) {
-                console.log(response);
-                //alert(response.responseText);
+                console.log(response);                
             });
         }
         
-        $scope.loadCardInfo = function (index) {            
-            
-                var encodedCardName = encodeURI($scope.cards[index].name);
+        $scope.loadCardInfo = function (index) {
 
-                $.get('https://jsonp.afeld.me/?url=http://yugiohprices.com/api/card_data/' + encodedCardName).success(function (response) {
-                    $timeout(function () {
-                        debugger;
-                        if (response.status != 'fail') {
-                            $scope.cards[index] = response.data;
-                            $scope.cards[index].image = 'http://yugiohprices.com/api/card_image/' + encodedCardName;
-                            $scope.cards[index].loaded = true;
-                        }
-                        else {
-                            $scope.cards[index].failed = true;
-                            $scope.cards[index].image = null;
-                        }
+            //Encode Card Name and get from A.P.I the data.
+            var encodedCardName = encodeURI($scope.cards[index].name);
 
-                        if (!$scope.dataLoaded) {
-                            $scope.selectedCard = $scope.cards[index];                            
-                            $scope.selectCard(index);
-                            $scope.dataLoaded = true;
-                        }
-                        
-                        //we do not use $.each, becasue a.p.i is too slow and looses data!!!!!
-                        if (index < $scope.cards.length) $scope.loadCardInfo(index + 1);
+            $.get('https://jsonp.afeld.me/?url=http://yugiohprices.com/api/card_data/' + encodedCardName).success(function (response) {
+                $timeout(function () {
+                    
+                    if (response.status != 'fail') {
+                        $scope.cards[index] = response.data;
+                        $scope.cards[index].image = 'http://yugiohprices.com/api/card_image/' + encodedCardName;
+                        //Set this flag to true so that we can hide the right pane if no data have been loaded.
+                        $scope.cards[index].loaded = true;
+                    }
+                    else {
+                        //Set this flag to false, if no data was found by calling the A.P.I
+                        $scope.cards[index].failed = true;
+                        $scope.cards[index].image = null;
+                    }
 
-                    }, 10);
-                });
-            
-            
+                    //ONLY on the first  load, select the default selected Card.
+                    if (!$scope.dataLoaded) {
+                        $scope.selectedCard = $scope.cards[index];
+                        $scope.selectCard(index);
+                        $scope.dataLoaded = true;
+                    }
+
+                    //we do not use $.each, becasue a.p.i is too slow and looses data!!!!!
+                    if (index < $scope.cards.length) $scope.loadCardInfo(index + 1);
+
+                }, 10);
+            });
         }
        
-        window.onresize = function () {            
+        window.onresize = function () {
+            //on each window resize, fix the height of list view.
             var pageHeight = $(window).height();
             var navHeight = pageHeight - 100;
             $('.size').css({ 'max-height': navHeight + 'px' });            
         }
 
+        //Load the inital data
         $scope.loadCardInfo(0);
         window.onresize();
     };
